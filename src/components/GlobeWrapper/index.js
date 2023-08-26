ada14f17e9fdfbe05bfc1d1f87559a952dcf43d3
Eylon Shmilovich <eylonshm@gmail.com>
2023-08-27 01:24:44 +0300

    Add Hexed Polygons example

--- src/components/GlobeWrapper/index.js
+++ src/components/GlobeWrapper/index.js
@@ -1,21 +1,32 @@
 import Globe from 'globe.gl'
 import { useEffect } from 'react'
+import { _GeoJSONLoader as GeoLoader } from '@loaders.gl/json'
+import countriesGeoJson from '../../datasets/countries.geojson'
+import { load } from '@loaders.gl/core'
 
 const GlobeWrapper = () => {
-  const N = 300
-  const gData = [...Array(N).keys()].map(() => ({
-    lat: (Math.random() - 0.5) * 180,
-    lng: (Math.random() - 0.5) * 360,
-    size: Math.random() / 3,
-    color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)],
-  }))
-
   useEffect(() => {
-    Globe()
-      .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
-      .pointsData(gData)
-      .pointAltitude('size')
-      .pointColor('color')(document.getElementById('globe'))
+    ;(async () => {
+      const countries = await load(countriesGeoJson, GeoLoader)
+      console.log('countries -> ', countries)
+      Globe()
+        .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
+        .hexPolygonsData(countries.features)
+        .hexPolygonResolution(3)
+        .hexPolygonMargin(0)
+        .hexPolygonColor(
+          () =>
+            `#${Math.round(Math.random() * Math.pow(2, 24))
+              .toString(16)
+              .padStart(6, '0')}`,
+        )
+        .hexPolygonLabel(
+          ({ properties: d }) => ` 
+            <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
+            Population: <i>${d.POP_EST}</i>
+          `,
+        )(document.getElementById('globe'))
+    })()
   }, [])
 
   return <div id='globe' />

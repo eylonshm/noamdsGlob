db39c9e8a1e5dbe883baadf4c82b65da41aa4112
Eylon Shmilovich <eylonshm@gmail.com>
2023-08-27 17:26:21 +0300

    Update globe style

--- src/components/GlobeWrapper/index.js
+++ src/components/GlobeWrapper/index.js
@@ -9,23 +9,30 @@ const GlobeWrapper = () => {
   useEffect(() => {
     ;(async () => {
       const countries = await load(countriesGeoJson, GeoLoader)
-      Globe()
+      const globe = Globe()
+      .backgroundImageUrl("//unpkg.com/three-globe/example/img/night-sky.png")
         .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
         .polygonsData(countries.features)
         .polygonCapMaterial((feature) => {
           const countryCode = feature.properties.ISO_A3
           return createCountryFlagMaterial(countryCode)
         })
+        .polygonStrokeColor(() => "rgba(0, 0, 0, 0.2)")
+        .polygonSideColor(() => "rgba(255, 255, 255, 0.02)")
+        .polygonCapColor(false)
         .polygonLabel(
-          ({ properties: d }) => ` 
+          ({ properties: d }) => `
             <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
             Population: <i>${d.POP_EST}</i>
           `,
         )(document.getElementById('globe'))
+
+        globe.controls().autoRotate = true
+        globe.controls().autoRotateSpeed = 0.2
     })()
   }, [])
 
-  return <div id='globe' />
+  return <div id='globe'/>
 }
 
 export default GlobeWrapper

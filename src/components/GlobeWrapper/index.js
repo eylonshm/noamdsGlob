bede7a25531b5bd80c4705863d4d6c77ae9c3a33
Eylon Shmilovich <eylonshm@gmail.com>
2023-08-27 13:45:31 +0300

    Rid of unneccessary code

--- src/components/GlobeWrapper/index.js
+++ src/components/GlobeWrapper/index.js
@@ -9,22 +9,13 @@ const GlobeWrapper = () => {
   useEffect(() => {
     ;(async () => {
       const countries = await load(countriesGeoJson, GeoLoader)
-      console.log('countries -> ', countries)
       Globe()
         .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
         .polygonsData(countries.features)
-        .polygonCapColor(
-          () =>
-            `#${Math.round(Math.random() * Math.pow(2, 24))
-              .toString(16)
-              .padStart(6, '0')}`,
-        )
         .polygonCapMaterial((feature) => {
           const countryCode = feature.properties.ISO_A3
           return createCountryFlagMaterial(countryCode)
         })
-        // We should use polygonCapMaterial here to render a 3js material
-        // which is a country flag image into the caps
         .polygonLabel(
           ({ properties: d }) => ` 
             <b>${d.ADMIN} (${d.ISO_A2})</b> <br />

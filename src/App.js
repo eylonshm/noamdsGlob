142b80e05bc09d20bfa46362834b50365ed5c4c0
Eylon Shmilovich <eylonshm@gmail.com>
2023-08-30 14:15:59 +0300

    Remove components index exports file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import './App.css'
-import { Globe } from './components'
+import Globe from './components/GlobeWrapper'
 import { useState, lazy, Suspense } from 'react'
 import Writer from './components/TypeWriter'
 const CountryModal = lazy(() => import('./components/CountryModal'))
-
 
 function App() {
   const [clickedCountry, setClickedCountry] = useState()
@@ -16,9 +15,13 @@ function App() {
     <div className='App'>
       <Globe setClickedCountry={setClickedCountry} />
       <Suspense>
-      <CountryModal open={clickedCountry} handleClose={onModalExit} countryCode={clickedCountry}/>
+        <CountryModal
+          open={clickedCountry}
+          handleClose={onModalExit}
+          countryCode={clickedCountry}
+        />
       </Suspense>
-      <Writer/>
+      <Writer />
       {/* <a href="https://www.linkedin.com/in/eylonshm" target="_blank" rel="noreferrer">
       <img src='/assets/logo.svg' style={{position: 'fixed', left: '1rem', top: '1rem', cursor: 'pointer', height: '4rem'}} alt=""/>
       </a> */}

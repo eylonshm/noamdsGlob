9cf4f237d539d0497d955b8f0dfc5cd629c7e45c
Eylon Shmilovich <eylonshm@gmail.com>
2023-08-30 14:17:13 +0300

    Remove globe log

--- src/components/GlobeWrapper/index.js
+++ src/components/GlobeWrapper/index.js
@@ -1,42 +1,37 @@
-import Globe from 'globe.gl';
-import { useEffect } from 'react';
-import { _GeoJSONLoader as GeoLoader } from '@loaders.gl/json';
-import countriesGeoJson from '../../datasets/countries.geojson';
-import { load } from '@loaders.gl/core';
-import createCountryFlagMaterial from '../CountryFlagMaterial';
+import Globe from 'globe.gl'
+import { useEffect } from 'react'
+import { _GeoJSONLoader as GeoLoader } from '@loaders.gl/json'
+import countriesGeoJson from '../../datasets/countries.geojson'
+import { load } from '@loaders.gl/core'
+import createCountryFlagMaterial from '../CountryFlagMaterial'
 
 const GlobeWrapper = ({ setClickedCountry }) => {
-    useEffect(() => {
-        (async () => {
-            const { features } = await load(countriesGeoJson, GeoLoader);
-            const globe = Globe()
-                .backgroundImageUrl(
-                    '//unpkg.com/three-globe/example/img/night-sky.png'
-                )
-                .globeImageUrl(
-                    '//unpkg.com/three-globe/example/img/earth-night.jpg'
-                )
-                .polygonsData(features)
-                .polygonCapMaterial(({ properties: { GU_A3, POSTAL } }) =>
-                    createCountryFlagMaterial(GU_A3, POSTAL)
-                )
-                .polygonStrokeColor(() => 'rgba(0, 0, 0, 0.2)')
-                .polygonSideColor(() => 'rgba(255, 255, 255, 0.02)')
-                .polygonCapColor(false)
-                .onPolygonClick((feature) => {
-                    console.log(feature);
-                    setClickedCountry(feature.properties.GU_A3);
-                })
-                .polygonLabel(
-                    ({ properties: d }) => `<b>${d.ADMIN}</b>`
-                )(document.getElementById('globe'));
+  useEffect(() => {
+    ;(async () => {
+      const { features } = await load(countriesGeoJson, GeoLoader)
+      const globe = Globe()
+        .backgroundImageUrl('//unpkg.com/three-globe/example/img/night-sky.png')
+        .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
+        .polygonsData(features)
+        .polygonCapMaterial(({ properties: { GU_A3, POSTAL } }) =>
+          createCountryFlagMaterial(GU_A3, POSTAL),
+        )
+        .polygonStrokeColor(() => 'rgba(0, 0, 0, 0.2)')
+        .polygonSideColor(() => 'rgba(255, 255, 255, 0.02)')
+        .polygonCapColor(false)
+        .onPolygonClick((feature) => {
+          setClickedCountry(feature.properties.GU_A3)
+        })
+        .polygonLabel(({ properties: d }) => `<b>${d.ADMIN}</b>`)(
+        document.getElementById('globe'),
+      )
 
-            globe.controls().autoRotate = true;
-            globe.controls().autoRotateSpeed = 0.2;
-        })();
-    }, []);
+      globe.controls().autoRotate = true
+      globe.controls().autoRotateSpeed = 0.2
+    })()
+  }, [])
 
-    return <div id="globe" />;
-};
+  return <div id='globe' />
+}
 
-export default GlobeWrapper;
+export default GlobeWrapper
